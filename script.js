60c028378b6bcec1693d4749ef8155e02087936a
rachidelaid <rachidelaid@gmail.com>
2021-12-14 12:31:52 +0100

    adding the javaScript code for the modal and to render the projects

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Mobile Menu
 const menu = document.querySelector('.menu');
 const links = document.querySelectorAll('.mobile-menu a');
 
@@ -18,4 +19,120 @@ links.forEach((a) => {
     menu.classList.remove('active');
     document.querySelector('.mobile-menu').style.display = 'none';
   });
+});
+
+// Project Popup
+const work = document.querySelector('#works');
+const projects = [
+  {
+    id: 1,
+    title: 'Tonic',
+    image: './assets/work-1.png',
+    imageAlt: 'First Project Image',
+    details: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    langs: ['html', 'css', 'javaScript'],
+    gitLink: '#',
+    srcLink: '#',
+  },
+  {
+    id: 2,
+    title: 'Multi-Post Stories',
+    image: './assets/work-2.png',
+    imageAlt: 'Second Project Image',
+    details: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    langs: ['html', 'css', 'javaScript'],
+    gitLink: '#',
+    srcLink: '#',
+  },
+  {
+    id: 3,
+    title: 'Tonic',
+    image: './assets/work-3.png',
+    imageAlt: 'Third Project Image',
+    details: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    langs: ['html', 'css', 'javaScript'],
+    gitLink: '#',
+    srcLink: '#',
+  },
+  {
+    id: 4,
+    title: 'Multi-Post Stories',
+    image: './assets/work-4.png',
+    imageAlt: 'Fourth Project Image',
+    details: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    langs: ['html', 'css', 'javaScript'],
+    gitLink: '#',
+    srcLink: '#',
+  },
+];
+
+for (let i = 0; i < projects.length; i++) {
+  const { title, image, imageAlt, details, description, langs, id } =
+    projects[i];
+
+  work.innerHTML += `
+    <div class="work-card" id="project-${id}">
+      <img src="${image}" alt="${imageAlt}" />
+      <div class="details">
+        <h2>${title}</h2>
+        <ul class="subTitle">
+          ${details.map((d) => `<li>${d}</li>`).join('')}
+        </ul>
+        <p>${description}</p>
+        <ul class="langauges">
+        ${langs.map((l) => `<li>${l}</li>`).join('')}
+        </ul>
+        <button>See Project</button>
+      </div>
+    </div>
+  `;
+}
+
+const modal = document.querySelector('.modal-wrapper');
+const close = document.querySelector('.close-modal');
+const titleElm = document.querySelector('.modal-title h2');
+const subTitle = document.querySelector('.modal .subTitle');
+const img = document.querySelector('.modal img');
+const paragraph = document.querySelector('.modal p');
+const languages = document.querySelector('.modal-content .langauges');
+const githubLink = document.querySelector('.modal-content .github-link');
+const sourceLink = document.querySelector('.modal-content .source-link');
+
+document.querySelectorAll('.work-card button').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const id = Number(btn.parentNode.parentNode.id.split('-')[1]);
+    const {
+      title,
+      image,
+      imageAlt,
+      details,
+      description,
+      langs,
+      gitLink,
+      srcLink,
+    } = projects.find((p) => p.id === id);
+
+    titleElm.innerText = title;
+    subTitle.innerHTML = details.map((d) => `<li>${d}</li>`).join('');
+    img.setAttribute('src', image);
+    img.setAttribute('alt', imageAlt);
+    paragraph.innerText = description;
+    languages.innerHTML = langs.map((l) => `<li>${l}</li>`).join('');
+    githubLink.setAttribute('href', gitLink);
+    sourceLink.setAttribute('href', srcLink);
+
+    modal.style.display = 'flex';
+  });
+});
+
+close.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
