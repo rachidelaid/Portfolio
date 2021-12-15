5787cb7ddca0c58542c35db091b95723cdd5b479
Billions Joel <joelatugonza@gmail.com>
2021-12-15 16:36:50 +0300

    Set local storage data back in form on load

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,18 +1,28 @@
 // storing the form data in local storage
 form.addEventListener('change', () => {
-  const obj = {};
+	const obj = {};
 
-  if (name.value.trim() !== '') {
-    obj.userName = name.value.trim();
-  }
+	if (name.value.trim() !== '') {
+		obj.userName = name.value.trim();
+	}
 
-  if (email.value.trim() !== '') {
-    obj.userEmail = email.value.trim();
-  }
+	if (email.value.trim() !== '') {
+		obj.userEmail = email.value.trim();
+	}
 
-  if (message.value.trim() !== '') {
-    obj.userMessage = message.value.trim();
-  }
+	if (message.value.trim() !== '') {
+		obj.userMessage = message.value.trim();
+	}
 
-  localStorage.setItem('formData', JSON.stringify(obj));
+	localStorage.setItem('formData', JSON.stringify(obj));
 });
+
+// set the localStorage data to the form
+
+let formObj = JSON.parse(localStorage.getItem('formData'));
+
+let { userName, userEmail, userMessage } = formObj;
+
+name.value = userName ? userName : '';
+email.value = userEmail ? userEmail : '';
+message.value = userMessage ? userMessage : '';
